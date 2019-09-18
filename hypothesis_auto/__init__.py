acc27752bd4d51861af47af90011a6a7a64a7f91
Timothy Crosley <timothy.crosley@gmail.com>
2019-09-17 21:44:49 -0700

    Split up functionality to enable multiple use modes

--- hypothesis_auto/__init__.py
+++ hypothesis_auto/__init__.py
@@ -1,3 +1,3 @@
-from hypothesis_auto.tester import auto_test, auto_test_module
+from hypothesis_auto.tester import auto_parameters, auto_test, auto_test_cases, auto_test_module
 
 __version__ = "0.0.5"
--- hypothesis_auto/tester.py
+++ hypothesis_auto/tester.py
@@ -1,9 +1,139 @@
 from inspect import isfunction, signature
 from types import ModuleType
-from typing import Callable, Optional, Tuple, Union, get_type_hints
+from typing import (
+    Any,
+    Callable,
+    Dict,
+    Generator,
+    List,
+    NamedTuple,
+    Optional,
+    Tuple,
+    Union,
+    get_type_hints,
+)
 
 from hypothesis.strategies import SearchStrategy, builds, just
 from pydantic import BaseModel
+
+
+class Parameters(NamedTuple):
+    """Represents the parameters meant to passed into a callable."""
+
+    args: List[Any]
+    kwargs: Dict[str, Any]
+
+
+class TestCase(NamedTuple):
+    """Represents an individual auto generated test case. To run the test case simply call() it."""
+
+    parameters: Parameters
+    test_function: Callable
+
+    def __call__(self) -> None:
+        """Calls the given test case returning None on success or Raising an exception on error"""
+        self.test_function(*self.parameters.args, **self.parameters.kwargs)
+
+
+def _test_function(
+    _auto_function: Callable,
+    _auto_verify: Callable = None,
+    _auto_allow_exceptions: Union[Tuple[BaseException], Tuple] = (),
+) -> Callable:
+    return_type = get_type_hints(_auto_function).get("return", None)
+
+    return_model = None
+    if return_type:
+
+        class ReturnModel(BaseModel):
+            __annotations__ = {"returns": return_type}
+
+        return_model = ReturnModel
+
+    def test_function(*args, **kwargs) -> None:
+        try:
+            result = _auto_function(*args, **kwargs)
+        except _auto_allow_exceptions:  # type: ignore
+            return
+
+        if return_model:
+            return_model(returns=result)
+        if _auto_verify:
+            _auto_verify(result)
+
+    return test_function
+
+
+def auto_parameters(
+    _auto_function: Callable, *args, _auto_limit: int = 50, **kwargs
+) -> Generator[Parameters, None, None]:
+    """Generates parameters from the given callable up to the specified limit
+    (`_auto_limit` parameter).
+
+    By default auto_parameters uses type annotations to automatically decide on strategies via the
+    hypothesis builds strategy. You can override individual strategies by passing them in under
+    the corresponding `*arg` or `**kwarg` OR you can pass in specific values that must be used for
+    certain parameters while letting others be auto generated.
+
+    All `*arg` and `**kwargs` are automatically passed along to
+    `hypothesis.strategies.builds` to enable this. Non strategies are automatically converted
+    to strategies using `hypothesis.strategies.just`.
+
+    Except for the following option:
+
+    - *_auto_limit*: Number of strategies combinations to run the given function against.
+    """
+    strategy_args = [arg if isinstance(arg, SearchStrategy) else just(arg) for arg in args]
+    strategy_kwargs = {
+        name: value if isinstance(value, SearchStrategy) else just(value)
+        for name, value in kwargs.items()
+    }
+
+    def pass_along_variables(*args, **kwargs):
+        return Parameters(args=args, kwargs=kwargs)
+
+    pass_along_variables.__signature__ = signature(_auto_function)  # type: ignore
+    pass_along_variables.__annotations__ = getattr(_auto_function, "__annotations__", {})
+    strategy = builds(pass_along_variables, *strategy_args, **strategy_kwargs)
+
+    for _ in range(_auto_limit):
+        yield strategy.example()
+
+
+def auto_test_cases(
+    _auto_function: Callable,
+    *args,
+    _auto_allow_exceptions: Union[Tuple[BaseException], Tuple] = (),
+    _auto_limit: int = 50,
+    _auto_verify: Optional[Callable] = None,
+    **kwargs
+) -> Generator[TestCase, None, None]:
+    """Generates test cases from the given callable up to the specified limit
+    (`_auto_limit` parameter).
+
+    By default auto_test_cases uses type annotations to automatically decide on strategies via the
+    hypothesis builds strategy. You can override individual strategies by passing them in under
+    the corresponding `*arg` or `**kwarg` OR you can pass in specific values that must be used for
+    certain parameters while letting others be auto generated.
+
+    All `*arg` and `**kwargs` are automatically passed along to
+    `hypothesis.strategies.builds` to enable this. Non strategies are automatically converted
+    to strategies using `hypothesis.strategies.just`.
+
+    Except for the following options:
+
+    - *_auto_allow_exceptions*: A tuple of exceptions that are acceptable for the function to raise
+      and will no be considered a test error.
+    - *_auto_limit*: Number of strategies combinations to run the given function against.
+    - *_auto_verify*: An optional callback function that will be called to allow custom verification
+      of the functions return value. The callback function should raise an AssertionError if the
+      return value does not match expectations.
+    """
+    test_function = _test_function(
+        _auto_function, _auto_verify=_auto_verify, _auto_allow_exceptions=_auto_allow_exceptions
+    )
+    for parameters in auto_parameters(_auto_function, *args, _auto_limit=_auto_limit, **kwargs):
+        yield TestCase(parameters=parameters, test_function=test_function)
 
 
 def auto_test(
@@ -35,39 +165,19 @@ def auto_test(
       of the functions return value. The callback function should raise an AssertionError if the
       return value does not match expectations.
     """
-    return_type = get_type_hints(_auto_function).get("return", None)
-
-    strategy_args = [arg if isinstance(arg, SearchStrategy) else just(arg) for arg in args]
-    strategy_kwargs = {
-        name: value if isinstance(value, SearchStrategy) else just(value)
-        for name, value in kwargs.items()
-    }
-
-    def pass_along_variables(*args, **kwargs):
-        return args, kwargs
-
-    pass_along_variables.__signature__ = signature(_auto_function)  # type: ignore
-    pass_along_variables.__annotations__ = getattr(_auto_function, "__annotations__", {})
-    strategy = builds(pass_along_variables, *strategy_args, **strategy_kwargs)
-
-    for _ in range(_auto_runs):
-        args, kwargs = strategy.example()
-        try:
-            result = _auto_function(*args, **kwargs)
-        except _auto_allow_exceptions:  # type: ignore
-            continue
-        if return_type:
-
-            class ReturnModel(BaseModel):
-                __annotations__ = {"returns": return_type}
-
-            ReturnModel(returns=result)
-        if _auto_verify:
-            _auto_verify(result)
+    for test_case in auto_test_cases(
+        _auto_function,
+        *args,
+        _auto_allow_exceptions=_auto_allow_exceptions,
+        _auto_limit=_auto_runs,
+        _auto_verify=_auto_verify,
+        **kwargs
+    ):
+        test_case()
 
 
 def auto_test_module(module: ModuleType) -> None:
-    """Attempts to automatically test every public function within a module."""
+    """Attempts to automatically test every public function within a module. For the brave only."""
     for attribute_name in dir(module):
         if not attribute_name.startswith("_"):
             attribute = getattr(module, attribute_name)
