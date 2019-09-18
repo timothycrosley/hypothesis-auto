153a106f75ccbc1845240706c7330a922cdcffc6
Timothy Crosley <timothy.crosley@gmail.com>
2019-09-17 22:39:14 -0700

    Add additional doc strings to pytest functionality

--- hypothesis_auto/pytest.py
+++ hypothesis_auto/pytest.py
@@ -15,6 +15,39 @@ def auto_pytest(
     _auto_verify: Optional[Callable] = None,
     **kwargs,
 ) -> None:
+    """A decorator that marks a parameterized pytest function passing along a callable test case.
+    The function should take a `test_case` parameter.
+
+    By default auto_pytest uses type annotations to automatically decide on strategies via the
+    hypothesis builds strategy. You can override individual strategies by passing them in under
+    the corresponding `*arg` or `**kwarg` OR you can pass in specific values that must be used for
+    certain parameters while letting others be auto generated.
+
+    All `*arg` and `**kwargs` are automatically passed along to
+    `hypothesis.strategies.builds` to enable this. Non strategies are automatically converted
+    to strategies using `hypothesis.strategies.just`.
+
+    Except for the following options:
+
+    - *_auto_allow_exceptions*: A tuple of exceptions that are acceptable for the function to raise
+      and will no be considered a test error.
+    - *_auto_runs*: Number of strategies combinations to run the given function against.
+    - *_auto_verify*: An optional callback function that will be called to allow custom verification
+      of the functions return value. The callback function should raise an AssertionError if the
+      return value does not match expectations.
+
+    Example:
+
+            def my_function(number_1: int, number_2: int) -> int:
+                return number_1 + number_2
+
+
+            @auto_pytest(my_function)
+            def test_auto_pytest(test_case):
+                test_case()
+
+    -----
+    """
     return pytest.mark.parametrize(
         "test_case",
         auto_test_cases(
@@ -35,6 +68,36 @@ def auto_pytest_magic(
     _auto_verify: Optional[Callable] = None,
     **kwargs,
 ) -> None:
+    """A convenience function that builds a new test function inside the calling module and
+    passes into it test cases using the `auto_pytest` decorator. The least effort and most magical
+    way to integrate with pytest.
+
+    By default auto_pytest_magic uses type annotations to automatically decide on strategies via the
+    hypothesis builds strategy. You can override individual strategies by passing them in under
+    the corresponding `*arg` or `**kwarg` OR you can pass in specific values that must be used for
+    certain parameters while letting others be auto generated.
+
+    All `*arg` and `**kwargs` are automatically passed along to
+    `hypothesis.strategies.builds` to enable this. Non strategies are automatically converted
+    to strategies using `hypothesis.strategies.just`.
+
+    Except for the following options:
+
+    - *_auto_allow_exceptions*: A tuple of exceptions that are acceptable for the function to raise
+      and will no be considered a test error.
+    - *_auto_runs*: Number of strategies combinations to run the given function against.
+    - *_auto_verify*: An optional callback function that will be called to allow custom verification
+      of the functions return value. The callback function should raise an AssertionError if the
+      return value does not match expectations.
+
+    Example:
+
+            def my_function(number_1: int, number_2: int) -> int:
+                return number_1 + number_2
+
+
+            auto_pytest_magic(my_function)
+    """
     called_from = inspect.stack()[1]
     module = inspect.getmodule(called_from[0])
 
